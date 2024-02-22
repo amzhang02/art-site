85e216e0e2654dbac18172df6839c7a6e80b82f9
amzhang02 <amzhang02@gmail.com>
2024-02-21 22:47:22 -0500

    fixed alt text for headshot

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   
   return (
     <>
-        <Image src='headshot.JPG' width={750} height={700} alt='Photo of a corner of an art studio.'/>
+        <Image src='headshot.JPG' width={750} height={700} alt='Photograph of Alyssa Zhang against geometric patterned background.'/>
       
 
     </>
